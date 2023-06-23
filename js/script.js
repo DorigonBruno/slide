fd4f4f65ff51071b77968c632254bc8675457772
Bruno <bdorigon@hotmail.com>
2023-06-23 18:57:02 -0300

    final navegacao prev e next

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ import Slide from "./slide.js";
 const slide = new Slide(".wrapper", ".slide");
 slide.init();
 
-slide.changeSlide(3)
+slide.changeSlide(3);
+slide.activePrevSlide()
--- js/slide.js
+++ js/slide.js
@@ -7,6 +7,10 @@ export default class Slide {
     this.bindEvents = this.bindEvents.bind(this);
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   updatePosition(clientX) {
     this.dist.movement = (this.dist.startX - clientX) * 1.4;
     return this.dist.finalPosition - this.dist.movement;
@@ -28,6 +32,7 @@ export default class Slide {
       moveType = "touchmove";
     }
     this.wrapper.addEventListener(moveType, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -43,6 +48,18 @@ export default class Slide {
     const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvents() {
@@ -86,8 +103,17 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
+  }
+
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
